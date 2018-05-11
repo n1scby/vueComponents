a04fbbc432dbc1e4d5594a4a9207bef8aeaa6c93
Nic B <n1scby@yahoo.com>
2018-05-11 09:50:39 -0400

    playing with components

--- components.js
+++ components.js
@@ -56,3 +56,67 @@ new Vue({
         ]
     }
 })
+
+
+
+Vue.component('task1', {
+
+    template:'<li>Foobar</li>'
+})
+
+new Vue({
+    el: '#task1'
+})
+
+
+Vue.component('task2', {
+    
+        template:'<li><slot></slot></li>'
+    })
+    
+    new Vue({
+        el: '#task2'
+    })
+
+
+Vue.component('task3', {
+
+    template:'<li>{{task}}</li>',
+    data() {
+        return {
+            task: "learn vue"
+        };
+    }
+});
+
+new Vue({
+    el: '#task3'
+})
+
+
+Vue.component('task-list1', {
+    // template:'<div><task2 v-for="task in tasks">{{task.task}}</task2></div>',
+    template:`
+             <div>
+                 <task2 v-for="task in tasks">{{task.task}}</task2>
+             </div>`,
+    data() {
+        return {
+            tasks: [
+                {task: 'Go to store', complete: true},
+                {task: 'Check Email', complete: false},
+                {task: 'Walk Dog', complete: true},
+                {task: 'Learn Vue', complete: false}
+            ]
+        };
+    }
+});
+
+
+new Vue({
+    el: '#tasks1'
+})
+
+
+
+
